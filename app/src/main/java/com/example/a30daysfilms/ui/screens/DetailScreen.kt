8fda6891d552258e7da0ce83835d62249e307df3
Fabian Ortiz Maestre <fmaestre98@gmail.com>
2023-07-07 20:58:16 -0400

    update compose version libraries and styles of details screen

--- app/src/main/java/com/example/a30daysfilms/ui/screens/DetailScreen.kt
+++ app/src/main/java/com/example/a30daysfilms/ui/screens/DetailScreen.kt
@@ -1,5 +1,6 @@
 package com.example.a30daysfilms.ui.screens
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.Orientation
@@ -8,23 +9,29 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -36,7 +43,9 @@ import coil.request.ImageRequest
 import com.example.a30daysfilms.R
 import com.example.a30daysfilms.data.remote.FilmsApi
 import com.example.a30daysfilms.domain.Film
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun DetailScreen(modifier: Modifier = Modifier, state: FilmViewModel.FilmsState) {
     val film: Film? = state.selectedFilm
@@ -55,16 +64,28 @@ fun DetailScreen(modifier: Modifier = Modifier, state: FilmViewModel.FilmsState)
             if (state.loadingImages) {
                 CircularProgressIndicator()
             } else if (state.imagesList?.isEmpty() == false) {
-                LazyRow(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
-                    items(state.imagesList) {
+
+                val pagerState = rememberPagerState()
+                val scope = rememberCoroutineScope()
+                Box(
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .height(330.dp)
+                ) {
+                    HorizontalPager(
+                        pageCount = state.imagesList.size,
+                        state = pagerState,
+                        key = { state.imagesList[it].file_path }) {
                         Card(
                             elevation = CardDefaults.cardElevation(8.dp),
                             shape = MaterialTheme.shapes.medium,
-                            modifier = Modifier.height(250.dp).width(200.dp)
+                            modifier = Modifier
+                                .height(250.dp)
+                                .width(200.dp)
                         ) {
                             AsyncImage(
                                 model = ImageRequest.Builder(context = LocalContext.current)
-                                    .data(FilmsApi.IMG_URL + it.file_path)
+                                    .data(FilmsApi.IMG_URL + state.imagesList[it].file_path)
                                     .crossfade(true)
                                     .build(),
                                 contentDescription = stringResource(R.string.image_description),
@@ -74,11 +95,56 @@ fun DetailScreen(modifier: Modifier = Modifier, state: FilmViewModel.FilmsState)
                                 modifier = Modifier.fillMaxSize()
                             )
                         }
-
                     }
 
+                    IconButton(
+                        onClick = {
+                            scope.launch {
+                                pagerState.animateScrollToPage(pagerState.currentPage + 1)
+                            }
+                        },
+                        modifier = Modifier
+                            .padding(15.dp)
+                            .align(Alignment.CenterEnd)
+                            .width(40.dp)
+                            .height(40.dp)
+                            .clip(
+                                CircleShape
+                            ),
+                        colors = IconButtonDefaults.iconButtonColors(containerColor = Color.White)
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.right_arrow_svgrepo_com),
+                            contentDescription = null,
+                            modifier = Modifier.padding(8.dp)
+                        )
+                    }
 
+                    IconButton(
+                        onClick = {
+                            scope.launch {
+                                pagerState.animateScrollToPage(pagerState.currentPage - 1)
+                            }
+                        },
+                        modifier = Modifier
+                            .padding(15.dp)
+                            .align(Alignment.CenterStart)
+                            .width(40.dp)
+                            .height(40.dp)
+                            .clip(
+                                CircleShape
+                            ),
+                        colors = IconButtonDefaults.iconButtonColors(containerColor = Color.White)
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.left_direction_arrow),
+                            contentDescription = null,
+                            modifier = Modifier.padding(8.dp)
+                        )
+                    }
                 }
+
+
             }
 
         }
